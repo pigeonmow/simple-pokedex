2c0b2e35b7251c049749d44da8ac3177ac651338
Matthew Moss <matt@learnetal.com>
2022-08-01 10:04:26 +0100

    Adds console command to import pokemon from API & associated model & migration for local storage in database

---
+++ app/Console/Commands/FetchPokemon.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Pokemon;
+use Illuminate\Console\Command;
+
+class FetchPokemon extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'pokemon:fetch';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Gets a list of all 151 original Pokemon from the Poke API and stores them in local database';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $data = $this->getData('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0');
+
+        if ($data['results']) {
+            $this->withProgressBar($data['results'], function ($result) {
+                Pokemon::create($result);
+            });
+        }
+
+        $this->info('The pokemon were imported!');
+    }
+
+    /**
+     * Gets data from API.
+     *
+     * @param $url
+     * @return mixed
+     */
+    private function getData($url)
+    {
+        $client = curl_init($url);
+
+        curl_setopt($client, CURLOPT_RETURNTRANSFER, true);
+
+        $response = curl_exec($client);
+
+        return json_decode($response, true);
+    }
+}
